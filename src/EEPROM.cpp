c4d88a7a0e2a04ef54727cc405493ba91e3253c7
Bram Soenens <bram@bram-rig.BramNet>
2020-03-12 22:31:15 +0100

    final comit for the day, uncompiled code

--- src/EEPROM.cpp
+++ src/EEPROM.cpp
@@ -3,7 +3,25 @@
 
 
 namespace Mamary{
-    EEPROM::EEPROM(){
-        
+    EEPROM::EEPROM(I2C &i2c_obj){
+        i2cbus = &i2c_obj;
+    }
+
+    char[] EEPROM::readData(char[] registerAddress, int size){
+        char dataRead[size];
+        i2cbus.write(addr, registerAddress, 2, true);
+        i2cbus.read(addr, dataRead, size);
+        return dataRead;
+    }
+
+    void EEPROM::writeData(char[] registerAddress, char[] writeData){
+        int arraySize = sizeof(writeData)/sizeof(writeData[0]);
+        char dataWrite[arraySize+2];
+        dataWrite[0] = registerAddress[0];
+        dataWrite[1] = registerAddress[1];
+        for( int i = 0; i< arraySize; i++){
+            dataWrite[i+2] = writeData[i];
+        }
+        i2cbus.write(addr, dataWrite, arraySize+2);
     }
 }
--- src/EEPROM.h
+++ src/EEPROM.h
@@ -1,8 +1,14 @@
 #pragma once
+#include "mbed.h"
 
 namespace Mamary{
     class EEPROM{
         public:
-            EEPROM();
+            EEPROM(I2C &i2c_obj);       // Here you have to give a pointer for the I2C object that is used for interfacing with the eeprom
+        private:
+            const char addr = 0x50 << 1;    // The only avalible I2C Address for our eeprom
+            char[] readData(char[] registerAddress, int size);
+            void writeData(char[] registerAddress, char[] writeData);
+            I2C &i2cbus;
     }
 }
