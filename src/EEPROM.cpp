dbc16ca2a7fb2b106fab608938ee0a919f57c078
Bram Soenens <bram@bram-rig.BramNet>
2020-03-20 09:24:55 +0100

    Won't compile if i2c-instance is passed

--- src/EEPROM.cpp
+++ src/EEPROM.cpp
@@ -3,25 +3,25 @@
 
 
 namespace Mamary{
-    EEPROM::EEPROM(I2C * i2c_obj){
+    EEPROM::EEPROM(I2C &i2c_obj){
         i2cbus(&i2c_obj);
     }
 
-    char EEPROM::readData(char registerAddress, unsigned int size){
-        char dataRead[size];
-        i2cbus.write(addr, registerAddress, 2, true);
-        i2cbus.read(addr, dataRead, size);
-        return dataRead;
+    void EEPROM::readData(char buffer, unsigned int length, unsigned int offset){
+        // char dataRead[size];
+        // i2cbus.write(addr, registerAddress, 2, true);
+        // i2cbus.read(addr, dataRead, size);
+        // return dataRead;
     }
 
-    void EEPROM::writeData(char registerAddress, char writeData){
-        unsigned int arraySize = sizeof(writeData)/sizeof(writeData[0]);
-        char dataWrite[arraySize+2];
-        dataWrite[0] = registerAddress[0];
-        dataWrite[1] = registerAddress[1];
-        for( unsigned int i = 0; i< arraySize; i++){
-            dataWrite[i+2] = writeData[i];
-        }
-        i2cbus.write(addr, dataWrite, arraySize+2);
+    void EEPROM::writeData(char buffer, unsigned int length, unsigned int offset){
+        // unsigned int arraySize = sizeof(writeData)/sizeof(writeData[0]);
+        // char dataWrite[arraySize+2];
+        // dataWrite[0] = registerAddress[0];
+        // dataWrite[1] = registerAddress[1];
+        // for( unsigned int i = 0; i< arraySize; i++){
+        //     dataWrite[i+2] = writeData[i];
+        // }
+        // i2cbus.write(addr, dataWrite, arraySize+2);
     }
 }
--- src/EEPROM.h
+++ src/EEPROM.h
@@ -4,9 +4,9 @@
 namespace Mamary{
     class EEPROM{
         public:
-            EEPROM(I2C * i2c_obj);       // Here you have to give a pointer for the I2C object that is used for interfacing with the eeprom
-            char readData(char registerAddress, unsigned int size);
-            void writeData(char registerAddress, char writeData);
+            EEPROM(I2C &i2c_obj);       // Here you have to give a pointer for the I2C object that is used for interfacing with the eeprom
+            void readData(char buffer, unsigned int length, unsigned int offset);
+            void writeData(char buffer, unsigned int length, unsigned int offset);
         private:
             const char addr = 0x50 << 1;    // The only avalible I2C Address for our eeprom
             I2C &i2c_obj;
