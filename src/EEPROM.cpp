6bba08fc7c7cf6036beb11923fd5ba2e2c18a58e
Bram Soenens <bram@bram-rig.BramNet>
2020-03-20 09:09:40 +0100

    added image

--- src/EEPROM.cpp
+++ src/EEPROM.cpp
@@ -3,23 +3,23 @@
 
 
 namespace Mamary{
-    EEPROM::EEPROM(I2C &i2c_obj){
-        i2cbus = &i2c_obj;
+    EEPROM::EEPROM(I2C * i2c_obj){
+        i2cbus(&i2c_obj);
     }
 
-    char[] EEPROM::readData(char[] registerAddress, int size){
+    char EEPROM::readData(char registerAddress, unsigned int size){
         char dataRead[size];
         i2cbus.write(addr, registerAddress, 2, true);
         i2cbus.read(addr, dataRead, size);
         return dataRead;
     }
 
-    void EEPROM::writeData(char[] registerAddress, char[] writeData){
-        int arraySize = sizeof(writeData)/sizeof(writeData[0]);
+    void EEPROM::writeData(char registerAddress, char writeData){
+        unsigned int arraySize = sizeof(writeData)/sizeof(writeData[0]);
         char dataWrite[arraySize+2];
         dataWrite[0] = registerAddress[0];
         dataWrite[1] = registerAddress[1];
-        for( int i = 0; i< arraySize; i++){
+        for( unsigned int i = 0; i< arraySize; i++){
             dataWrite[i+2] = writeData[i];
         }
         i2cbus.write(addr, dataWrite, arraySize+2);
--- src/EEPROM.h
+++ src/EEPROM.h
@@ -4,11 +4,12 @@
 namespace Mamary{
     class EEPROM{
         public:
-            EEPROM(I2C &i2c_obj);       // Here you have to give a pointer for the I2C object that is used for interfacing with the eeprom
+            EEPROM(I2C * i2c_obj);       // Here you have to give a pointer for the I2C object that is used for interfacing with the eeprom
+            char readData(char registerAddress, unsigned int size);
+            void writeData(char registerAddress, char writeData);
         private:
             const char addr = 0x50 << 1;    // The only avalible I2C Address for our eeprom
-            char[] readData(char[] registerAddress, int size);
-            void writeData(char[] registerAddress, char[] writeData);
-            I2C &i2cbus;
-    }
-}
+            I2C &i2c_obj;
+            I2C i2cbus;
+    };
+};
